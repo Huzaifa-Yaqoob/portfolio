a77b2a2071115310631550a1e1b8747768cfdb49
Huzaifa-Yaqoob <muhammadhuzaifayaqoob3@gmail.com>
2023-11-17 22:46:11 +0500

    Added responsive navbar and footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import { FaLinkedin, FaXTwitter } from "react-icons/fa6";
+import { FiDribbble } from "react-icons/fi";
+import { HiMail, HiPhone, HiLocationMarker } from "react-icons/hi";
+import { pageLinks } from "../navbar/Navbar";
+import { Button } from "../ui/button";
+import GlowingButton from "../common/GlowingButton";
+
+const socialLinks = [
+  { name: "Linkedin", href: "/", icon: FaLinkedin },
+  { name: "X", href: "/", icon: FaXTwitter },
+  { name: "Dribbble", href: "/", icon: FiDribbble },
+];
+
+const contactInfo = [
+  { name: "mail", icon: HiMail, info: "Nextdev@gmail.com" },
+  { name: "phone", icon: HiPhone, info: "+92 1234567899" },
+  { name: "location", icon: HiLocationMarker, info: "Somewhere in the World" },
+];
+
+export default function Footer() {
+  return (
+    <footer className="my-container border-t-[1px] space-y-4 py-2">
+      <div className="flex flex-col md:flex-row gap-4 md:justify-between items-center">
+        <div className="text-primary font-semibold order-1">
+          Jeffery Cannon.
+        </div>
+        <div className="order-3 md:order-2">
+          {pageLinks.map((link) => (
+            <Button
+              key={link.href}
+              asChild
+              variant="link"
+              className="text-greyShades-g20"
+            >
+              <Link href={link.href}>{link.name}</Link>
+            </Button>
+          ))}
+        </div>
+        <div className="flex gap-4 order-2 md:order-3">
+          {socialLinks.map((link) => (
+            <Link key={link.name} href={link.href}>
+              <GlowingButton Icon={link.icon} />
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="hidden justify-center md:flex">
+        <div className="p-2 flex gap-6 items-center border-[1px] rounded">
+          {contactInfo.map((item) => (
+            <div key={item.name} className="flex gap-2 items-center">
+              <item.icon className="text-primary" />
+              <span className="">{item.info}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="text-greyShades-g50 text-center">
+        Copyright © 2023 Jeffery Cannon. All rights reserved.
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/navbar/LinksPopover.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { HiX, HiOutlineMenuAlt3 } from "react-icons/hi";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { type PageLinks } from "./Navbar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "../ui/button";
+import BookACallButton from "../common/BookACallButton";
+
+interface LinksBlockProps {
+  links: Array<PageLinks>;
+}
+
+export default function LinksPopover({ links }: LinksBlockProps) {
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
+
+  return (
+    <div className="block md:hidden">
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger>
+          <div className="text-2xl">
+            {open ? <HiX /> : <HiOutlineMenuAlt3 />}
+          </div>
+        </PopoverTrigger>
+        <PopoverContent className="w-fit bg-peachShades-p85 border-none mt-4">
+          <div className="space-y-4">
+            <div className="flex flex-col items-start gap-2">
+              {links.map((link) => (
+                <Button
+                  key={link.href}
+                  asChild
+                  variant={link.href === pathname ? "secondary" : "ghost"}
+                  className={`w-full ${
+                    link.href === pathname ? "shadow-sm" : ""
+                  }`}
+                  onClick={() => setOpen(false)}
+                >
+                  <Link href={link.href} className="text-greyShades-g20">
+                    {link.name}
+                  </Link>
+                </Button>
+              ))}
+            </div>
+            <BookACallButton />
+          </div>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
