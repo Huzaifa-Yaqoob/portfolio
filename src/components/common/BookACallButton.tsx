d3b2c75f783ff1e8f942b2b6bdaa89565199effd
Huzaifa-Yaqoob <muhammadhuzaifayaqoob3@gmail.com>
2023-11-18 23:16:43 +0500

    addedd animation on arrowbutton and bookacall button

--- src/components/common/BookACallButton.tsx
+++ src/components/common/BookACallButton.tsx
@@ -1,10 +1,29 @@
-import { Button } from "../ui/button";
+"use client";
+
+import { useAnimate } from "framer-motion";
 import { HiPhone } from "react-icons/hi";
+import { Button } from "../ui/button";
 
 export default function BookACallButton() {
+  const [scope, animate] = useAnimate();
+
   return (
-    <Button className="space-x-1 shadow ">
-      <HiPhone />
+    <Button
+      className="space-x-1 shadow"
+      onMouseOver={() =>
+        animate(
+          scope.current,
+          {
+            rotate: [15, 0, 15],
+          },
+          { repeat: Infinity, repeatType: "loop", duration: 0.5 }
+        )
+      }
+      onMouseLeave={() => animate(scope.current, { rotate: 0 })}
+    >
+      <span ref={scope}>
+        <HiPhone />
+      </span>
       <span>Book a call</span>
     </Button>
   );
--- src/components/common/ButtonWithArrow.tsx
+++ src/components/common/ButtonWithArrow.tsx
@@ -1,4 +1,6 @@
-import Link from "next/link";
+"use client";
+
+import { useAnimate } from "framer-motion";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { Button } from "../ui/button";
 
@@ -13,18 +15,31 @@ interface ButtonWithArrowProps {
     | null
     | undefined;
   text?: string;
-  href?: string;
 }
 
 export default function ButtonWithArrow({
   variant = "ghost",
   text = "View Portfolio",
-  href = "/portfolio",
 }: ButtonWithArrowProps) {
+  const [scope, animate] = useAnimate();
+
   return (
-    <Button variant={variant} className="flex items-center gap-1">
+    <Button
+      variant={variant}
+      className="flex items-center gap-1"
+      onMouseOver={() =>
+        animate(scope.current, {
+          x: 10,
+          rotate: -60,
+          y: [2, -2],
+        })
+      }
+      onMouseLeave={() => animate(scope.current, { x: 0, rotate: 0, y: 0 })}
+    >
       {text}
-      <HiArrowNarrowRight className="" />
+      <span ref={scope}>
+        <HiArrowNarrowRight />
+      </span>
     </Button>
   );
 }
