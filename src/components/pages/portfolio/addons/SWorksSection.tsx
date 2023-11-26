ac9b46a576161001c4828af9080c398cb6831389
Huzaifa-Yaqoob <51huzaifayaqoob@gmail.com>
2023-11-26 19:43:41 +0500

    change the structure of data in caraousal data for homepage

--- src/components/pages/portfolio/addons/SWorksSection.tsx
+++ src/components/pages/portfolio/addons/SWorksSection.tsx
@@ -2,46 +2,53 @@ import { worksCarousalData } from "@/data/worksData";
 import ButtonWithArrow from "@/components/common/ButtonWithArrow";
 import WorksCarousal from "@/components/common/carousals/WorksCarousal";
 import { Separator } from "@/components/ui/separator";
+import Reveal from "@/components/animations/Reveal";
 
 export default function SWorksSection() {
   return (
     <div className="space-y-16 block md:hidden">
-      <div className="space-y-8">
-        <h3 className="text-center md:text-left">
-          {worksCarousalData.works2023.title}
-        </h3>
-        <Separator />
-        <WorksCarousal
-          carousalData={worksCarousalData.works2023.carousalData.slice(0, 3)}
-        />
-        <div className="flex justify-center items-center">
-          <ButtonWithArrow text="View All Projects" variant={"default"} />
+      <Reveal classes="">
+        <div className="space-y-8">
+          <h3 className="text-center md:text-left">
+            {worksCarousalData.works2023.title}
+          </h3>
+          <Separator />
+          <WorksCarousal
+            carousalData={worksCarousalData.works2023.carousalData.slice(0, 3)}
+          />
+          <div className="flex justify-center items-center">
+            <ButtonWithArrow text="View All Projects" variant={"default"} />
+          </div>
         </div>
-      </div>
-      <div className="space-y-8">
-        <h3 className="text-center md:text-left">
-          {worksCarousalData.works2022.title}
-        </h3>
-        <Separator />
-        <WorksCarousal
-          carousalData={worksCarousalData.works2022.carousalData.slice(0, 3)}
-        />
-        <div className="flex justify-center items-center">
-          <ButtonWithArrow text="View All Projects" variant={"default"} />
+      </Reveal>
+      <Reveal classes="">
+        <div className="space-y-8">
+          <h3 className="text-center md:text-left">
+            {worksCarousalData.works2022.title}
+          </h3>
+          <Separator />
+          <WorksCarousal
+            carousalData={worksCarousalData.works2022.carousalData.slice(0, 3)}
+          />
+          <div className="flex justify-center items-center">
+            <ButtonWithArrow text="View All Projects" variant={"default"} />
+          </div>
         </div>
-      </div>
-      <div className="space-y-8">
-        <h3 className="text-center md:text-left">
-          {worksCarousalData.works2021.title}
-        </h3>
-        <Separator />
-        <WorksCarousal
-          carousalData={worksCarousalData.works2021.carousalData.slice(0, 3)}
-        />
-        <div className="flex justify-center items-center">
-          <ButtonWithArrow text="View All Projects" variant={"default"} />
+      </Reveal>
+      <Reveal classes="">
+        <div className="space-y-8">
+          <h3 className="text-center md:text-left">
+            {worksCarousalData.works2021.title}
+          </h3>
+          <Separator />
+          <WorksCarousal
+            carousalData={worksCarousalData.works2021.carousalData.slice(0, 3)}
+          />
+          <div className="flex justify-center items-center">
+            <ButtonWithArrow text="View All Projects" variant={"default"} />
+          </div>
         </div>
-      </div>
+      </Reveal>
     </div>
   );
 }
