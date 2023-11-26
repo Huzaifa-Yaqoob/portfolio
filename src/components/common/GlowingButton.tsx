c5b52fda6541c7f12bf78d4c968db2be12cf6aed
Huzaifa-Yaqoob <51huzaifayaqoob@gmail.com>
2023-11-26 13:14:55 +0500

    added page not found loading and error component for a web app

--- src/components/common/GlowingButton.tsx
+++ src/components/common/GlowingButton.tsx
@@ -5,14 +5,18 @@ import { type IconType } from "react-icons";
 interface GlowingButtonProps {
   Icon: IconType;
   size?: string;
+  color?: string;
 }
 
 export default function GlowingButton({
   Icon,
   size = "",
+  color = "bg-primary",
 }: GlowingButtonProps): React.ReactElement {
   return (
-    <div className="bg-primary p-2 rounded text-primary-foreground shadow-inerglow shadow-peachShades-p99 hover:shadow-inerglowRemove transition-shadow duration-500 w-fit">
+    <div
+      className={`${color} p-2 rounded text-primary-foreground shadow-inerglow shadow-peachShades-p99 hover:shadow-inerglowRemove transition-shadow duration-500 w-fit`}
+    >
       <Icon className={size} />
     </div>
   );
